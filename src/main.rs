5bba868607c26932bbaae42229fd9f111fa2716a
cloudlein <ayubi54321@gmail.com>
2026-03-27 23:13:00 +0700

    feat: Implement a basic Todo API server using Axum with CRUD operations.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use axum::extract::{Path, State};
+use axum::http::StatusCode;
+use axum::routing::{delete, get, post, put};
+use axum::{Json, Router};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use axum::extract::State;
-use axum::{Json, Router};
-use axum::routing::{get, post};
-use serde::{Deserialize, Serialize};
+use tokio::task::id;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Todo {
@@ -23,6 +25,8 @@ async fn main() {
         .route("/", get(root))
         .route("/todos", get(get_todos))
         .route("/todos", post(create_todos))
+        .route("/todos/:id", put(update_todos))
+        .route("/todos/:id", delete(delete_todos))
         .with_state(todos);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -54,4 +58,41 @@ async fn create_todos(
     let mut todos = todos.lock().unwrap();
     todos.push(payload.clone());
     Json(payload)
+}
+
+async fn update_todos(
+    Path(id): Path<u32>,
+    State(todos): State<SharedTodos>,
+    Json(payload): Json<Todo>
+) -> Result<Json<Todo>, StatusCode> {
+    let mut todos = todos.lock().unwrap();
+
+    for todo in todos.iter_mut()  {
+        if todo.id == id {
+            todo.title = payload.title.clone();
+            todo.completed = payload.completed;
+
+            return Ok(Json(todo.clone()));
+        }
+
+    }
+
+    Err(StatusCode::NOT_FOUND)
+}
+
+async fn delete_todos(
+    Path(id): Path<u32>,
+    State(todos): State<SharedTodos>,
+) -> StatusCode {
+    let mut todos = todos.lock().unwrap();
+
+    let previous_len = todos.len();
+
+    todos.retain(|todo| todo.id != id);
+
+    if todos.len() < previous_len {
+        StatusCode::NO_CONTENT
+    }else {
+        StatusCode::NOT_FOUND
+    }
 }
